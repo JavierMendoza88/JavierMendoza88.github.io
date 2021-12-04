e238d4accb4896be5294fc0c1ad593b0d82776d0
JavierMendoza88 <80964891+JavierMendoza88@users.noreply.github.com>
2021-12-04 15:40:08 -0500

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,27 +1,36 @@
 /* Function that is executed in place and used to handle the YouTube
    video popups and closures. In this case we use the jQuery as an object
-   and pass it in as a reference, dented by $. */
-$(function($) {
+   and pass it in as a reference, identified by $. 
+*/
 
-	var $window = $(window),
-		$body = $('body');
+$(function($){
+	var $window = $(window);
+	var	$body = $('body');
 
-	// Poptrox. Called when the web page is loaded.
-		$window.on('load', function() {
-			// This variable will be used to handle the thumbnails element from the HTML
-			var $thumbnails = $('.thumbnails');
-
-			if ($thumbnails.length > 0)
-				$thumbnails.poptrox({
-					// Called when the YouTube popup closes
-					onPopupClose: function() { $body.removeClass('is-covered'); },
-					// Called when the YouTube popup opens
-					onPopupOpen: function() { $body.addClass('is-covered'); },
-					// Color that is overlayed on the main window. Helpful to use
-					// So background doesn't distract from video popup
-					overlayColor: 'darkgray',
-					// If true, then default popup styling will be used
-					usePopupDefaultStyling: false,
-				});
+	// Called when the page is loaded, uses methods from poptrox library
+	$window.on('load', function() {
+		var $thumbnails = $('.thumbnails');
+		$thumbnails.poptrox({
+			// Called when the YouTube popup closes
+			onPopupClose: function() { $body.removeClass('is-covered'); },
+			// Called when the YouTube popup opens
+			onPopupOpen: function() { $body.addClass('is-covered'); },
+			// Color that is overlayed on the main web page. Helpful to use
+			// So background doesn't distract from video popup
+			overlayColor: 'darkgray',
+			// If true, then default popup styling will be used
+			usePopupDefaultStyling: false,
 		});
-})(jQuery); // return jQuery object
+	});
+
+	// function to slowly scroll back to top of web page using jQuery methods
+	$('.scroll_to').click(function(e) {
+		var scroll = $(this).attr('href');
+		var position = $(scroll).offset();
+		$('html, body').stop().animate({
+		   scrollTop: position.top
+		}, 2000);
+		e.preventDefault();
+	 });
+	
+});
